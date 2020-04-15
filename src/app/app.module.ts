8885c2813adb502254afe9a0066ca4afe8db0dd5
thehanir95 <thehanicis95@gmail.com>
2020-04-15 12:58:16 +0530

    added files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
@@ -11,12 +16,12 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatFormFieldModule , MatFormFieldControl} from '@angular/material/form-field';
 
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
+import {MatInputModule } from '@angular/material/input';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
 
@@ -29,7 +34,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatTreeModule} from '@angular/material/tree';
-import {MatIconModule} from '@angular/material';
+import {MatIconModule , MatChipsModule} from '@angular/material';
 import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 
 
@@ -43,7 +48,9 @@ import { SignupComponent } from './signup/signup.component';
   declarations: [
     AppComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+
+  
   ],
   imports: [
     BrowserModule,
@@ -73,13 +80,22 @@ import { SignupComponent } from './signup/signup.component';
     MatTabsModule,
     MatTreeModule,
     MatIconModule,
-    FormsModule,ReactiveFormsModule
+    FormsModule,ReactiveFormsModule,
+
+    CommonModule,
+    RouterModule,
+
+    MatChipsModule,
+    
     
 
-
-
   ],
+
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ],
+  schemas: [
+  ],
 })
 export class AppModule { }
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 
+import {FocusMonitor} from '@angular/cdk/a11y';
+import {coerceBooleanProperty} from '@angular/cdk/coercion';
+import {ElementRef, Input, OnDestroy, Optional, Self} from '@angular/core';
+import {FormBuilder, FormGroup, ControlValueAccessor, NgControl} from '@angular/forms';
+import {MatFormFieldControl} from '@angular/material/form-field';
+import {Subject} from 'rxjs';
+
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-
+  hide = true;
   email = new FormControl('', [Validators.required, Validators.email]);
 
   constructor() { }
@@ -23,6 +31,4 @@ export class SigninComponent implements OnInit {
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
-
-
 }
